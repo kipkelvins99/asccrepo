a72cc0be7c45595c57c5b06471d00a4e55cb8456
RosmyJohn20 <rosmyjohnthokkudam@gamil.com>
2022-06-22 16:27:01 +0530

    [ADD]

---
+++ customer_follow_up/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Customer Followup Report',
+    'version': '14.0.1.0.1',
+    'summary': 'To handle the followup report in customer followup report',
+    'author': 'Cybrosys Techno solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'depends': ['base','account_followup'],
+    'maintainer': 'Cybrosys Techno Solutions',
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
---
+++ customer_follow_up/models/__init__.py
@@ -0,0 +1,1 @@
+from . import account_follow_up_customer
