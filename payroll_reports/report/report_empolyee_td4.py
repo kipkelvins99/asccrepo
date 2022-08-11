b1fda1bf5dd899564068065cc91ca5c38da12a74
CybroOdooDev <cybroodoo@gmail.com>
2022-08-11 16:54:57 +0530

    [updations deductions]payroll reports

--- payroll_reports/report/report_empolyee_td4.py
+++ payroll_reports/report/report_empolyee_td4.py
@@ -7,7 +7,6 @@ class TD4Report(models.AbstractModel):
     @api.model
     def _get_report_values(self, docids, data=None):
         docs = self.env['hr.employee'].browse(data['context']['active_ids'])
-        print('pppppppp',docs)
         return {
             'doc_ids': docids,
             'doc_model': 'hr.employee',
--- payroll_reports/wizard/td4_report_wizard.py
+++ payroll_reports/wizard/td4_report_wizard.py
@@ -9,28 +9,12 @@ class td4ReportWizard(models.TransientModel):
     _name = 'td4.report.wizard'
     _description = 'TD4 Report'
 
-    date_start = fields.Date('Start Period', required=True,
-                             default=lambda self: (fields.Date.today() - relativedelta(years=1)).replace(month=1,
-                                                                                                         day=1),
-                             help="Start date of the period to consider.")
-    date_end = fields.Date('End Period', required=True,
-                           default=lambda self: (fields.Date.today() - relativedelta(years=1)).replace(month=12,
-                                                                                                       day=31),
-                           help="End date of the period to consider.")
     employee_ids = fields.Many2many('hr.employee', string='Employees', required=1)
-    year = fields.Selection([(str(y), str(y)) for y in range(1970, datetime.now().year+1)], 'Year', required=True)
+    year = fields.Selection([(str(y), str(y)) for y in range(1990, datetime.now().year+1)], 'Year', required=True)
 
     def print_report(self):
-
-        if self.date_end.year != self.date_start.year:
-            raise ValidationError("Invalid year selected")
-
-        total_weeks = self.weeks_for_year(self)
+        last_week = date(self, 12, 28)
+        total_weeks = last_week.isocalendar()[1]
         data = {'year': self.year, 'total_weeks': total_weeks}
         return self.env.ref('payroll_reports.employee_td4_report').report_action(self.employee_ids, data=data)
 
-    def weeks_for_year(self,year):
-        print(self,year, 'oooo')
-        last_week = date(year, 12, 28)
-        print(last_week.isocalendar()[1], 'last week')
-        return last_week.isocalendar()[1]
