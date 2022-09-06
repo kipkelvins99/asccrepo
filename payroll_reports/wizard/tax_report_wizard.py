6976bcc2d8029cacd947e8a8a4d4ea525f006a48
CybroOdooDev <cybroodoo@gmail.com>
2022-09-06 16:52:34 +0530

    [payroll addition/deduction report completed]payroll reports

--- payroll_reports/wizard/tax_report_wizard.py
+++ payroll_reports/wizard/tax_report_wizard.py
@@ -12,10 +12,10 @@ class TaxReportWizard(models.TransientModel):
                              ('5', 'May'), ('6', 'June'), ('7', 'July'), ('8', 'August'),
                              ('9', 'September'), ('10', 'October'), ('11', 'November'), ('12', 'December')], 'Month')
 
-    def additions_report(self):
+    def print(self):
         data = {'employee_ids': self.employee_ids.ids, 'year': self.year, 'month': self.month}
         return self.env.ref('payroll_reports.payroll_addition_report').report_action(self.employee_ids, data=data)
 
-    def deductions_report(self):
-        data = {'employee_ids': self.employee_ids.ids, 'year': self.year, 'month': self.month}
-        return self.env.ref('payroll_reports.payroll_deduction_report').report_action(self.employee_ids, data=data)
+    # def deductions_report(self):
+    #     data = {'employee_ids': self.employee_ids.ids, 'year': self.year, 'month': self.month}
+    #     return self.env.ref('payroll_reports.payroll_deduction_report').report_action(self.employee_ids, data=data)
