b54d1e99f98534d975d01a272c1e3d0f96eb6061
Cybrosys Odoo Devs <50653951+CybroOdooDev@users.noreply.github.com>
2022-11-23 16:53:19 +0530

    Delete __init__.py

--- payroll_reports/report/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import summary_report
-from . import payroll_addition_report
-from . import payroll_deduction_report
-from . import monthly_tax_report
-from . import nis_report
-from . import monthly_payroll_summary_report
-from . import nis_report_due
