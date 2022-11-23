e17a92e9fa4cbba800a9b557d58baebfc07dd9a8
Cybrosys Odoo Devs <50653951+CybroOdooDev@users.noreply.github.com>
2022-11-23 17:13:42 +0530

    Delete stock_return_picking.py

--- stock_return_picking/models/stock_return_picking.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class StockReturnPickingUpdatedLine(models.TransientModel):
-    _inherit = "stock.return.picking.line"
-
-    to_refund = fields.Boolean(string="Update quantities on SO/PO",
-                               default=False,
-                               help='Trigger a decrease of the delivered/received quantity in the associated Sale Order/Purchase Order')
